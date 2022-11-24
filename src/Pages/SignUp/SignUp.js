44150612d779bf8d91d6087c44ac9bc23210a373
SamitMahadi <samithossainmahadi@gmail.com>
2022-11-24 13:03:20 -0500

    sign up and login page done

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,25 +1,39 @@
-import React, { useState } from 'react';
-import lcar from '../../Assets/lcar.png'
-import { useForm } from 'react-hook-form';
+import React, { useEffect, useState } from 'react';
+import { useForm, Controller } from 'react-hook-form';
 import { Link } from 'react-router-dom';
-const Login = () => {
+import lambo from '../../Assets/Signupcar.png'
+
+const SignUp = () => {
+
     const { register, formState: { errors }, handleSubmit } = useForm();
-    const handleLogin = data =>{
+
+
+    const handleLogin = data => {
         console.log(data);
     }
-
 
 
 
     return (
         <div>
             <div className="hero mt-16 mb-16">
-                <div className="hero-content  flex-col lg:flex-row flex justify-between">
-                    <img src={lcar} className="lg:w-1/2 rounded-lg  " alt='' />
+                <div className="hero-content  flex-col lg:flex-row flex">
+                    <img src={lambo} className="lg:w-1/2 rounded-lg  " alt='' />
                     <div className='pl-4'>
                         <div className='p-8 card shadow-xl rounded-lg'>
-                            <h2 className='text-3xl text-center'>Login</h2>
+                            <h2 className='text-3xl text-center'>Sign Up</h2>
                             <form onSubmit={handleSubmit(handleLogin)}>
+
+                                <div className="form-control w-full max-w-xs mb-3">
+
+                                    <label className="label"><span className="label-text">Name</span></label>
+                                    <input type="text"{...register("name", {
+                                        required: "Name Is Required"
+                                    })} className="input input-bordered w-full max-w-xs" />
+                                    {errors.name && <p className='text-red-700' role="alert">{errors.name?.message}</p>}
+
+
+                                </div>
 
 
                                 <div className="form-control w-full max-w-xs mb-3">
@@ -40,14 +54,35 @@ const Login = () => {
 
                                         })} />
                                     {errors.password && <p className='text-red-700' role="alert">{errors.password?.message}</p>}
-                                    <label className="label"><span className="label-text">forget password?</span></label>
+
 
                                 </div>
+
+
+
+                                <div className="form-control w-full max-w-xs">
+
+                                    <select className="select select-bordered w-full max-w-xs mt-6 mb-6"{...register("role",{
+                                        required:"your role is required"
+                                    })}>
+                                        <option disabled selected>What Are You?</option>
+                                        <option>Buyer</option>
+                                        <option>Seller</option>
+                                    </select>
+                                    {errors.role && <p className='text-red-700' role="alert">{errors.role?.message}</p>}
+
+
+                                </div>
+
+
+
+
+
 
                                 <input className='btn  bg-red-700 text-white w-full' value="Login" type="submit" />
                                 {/* login erro set korte hbbe */}
                             </form>
-                            <p className='mt-3'>New to Car Zone? <Link className='text-red-700' to='/signup'>Create New Account</Link> </p>
+                            <p className='mt-3'>Already Have An Account? <Link className='text-red-700' to='/login'>Login Now</Link> </p>
                             <div className="divider">OR</div>
                             <button className="btn btn-outline w-full font-uppercase hover: text-white  bg-red-700 ">continue with google</button>
                         </div>
@@ -58,4 +93,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default SignUp;
